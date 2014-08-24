3659c4661327671f9d85bc9a74326bf2987ceb95
Sean Azlin <sazlin@gmail.com>
2014-08-24 15:24:14 -0700

    Enabled admin functionality.

--- offerup_challenge_site/settings.py
+++ offerup_challenge_site/settings.py
@@ -122,8 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'storages',
     'image_upload_app',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
