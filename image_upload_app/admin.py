36a25231b068b17c7042ca31d0a0fae1d791411a
Sean Azlin <sazlin@gmail.com>
2014-08-25 15:51:08 -0700

    Customized the admin interface for image's to show a useful list view.

--- image_upload_app/admin.py
+++ image_upload_app/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from image_upload_app.models import Image
 
-admin.site.register(Image)
+
+class ImageAdmin(admin.ModelAdmin):
+    def image_link(self, image):
+        return u"<a href='{}'>{}</a>".format(image.fileName.url, image.fileName)
+    image_link.allow_tags = True
+
+    fields = ('id', 'fileName', 'Duplicate', 'Hash')
+    list_display = ('id', 'image_link', 'Duplicate', 'Hash')
+    readonly_fields = ('id', 'fileName', 'Duplicate', 'Hash')
+
+admin.site.register(Image, ImageAdmin)
