a25f79739186065afa67580344827b18b2d03b01
Sean Azlin <sazlin@gmail.com>
2014-08-25 19:54:17 -0700

    Added a post_delete handler for Image to delete the actual file from s3.

--- image_upload_app/models.py
+++ image_upload_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from image_upload_app.tasks import checkForDuplicates
 
@@ -19,5 +19,15 @@ class Image(models.Model):
 @receiver(signal=post_save, sender=Image)
 def image_post_save(sender, instance, created, *args, **kwargs):
     if created:
-        print 'image created --> checking for dupes'
+        print 'image created --> checking for dupes for ', instance.id
         checkForDuplicates.delay(instance.id)
+
+
+@receiver(signal=post_delete, sender=Image)
+def image_post_delete_(sender, instance, **kwargs):
+    print 'image deleted --> removing file from s3 ', instance.id
+    instance.fileName.delete(False)
+
+    # The following two line also work
+    # storage, name = instance.fileName.storage, instance.fileName.name
+    # storage.delete(name)
