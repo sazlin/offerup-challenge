df30836b1a303103bfee2b326d8ef75984d7dd46
Sean Azlin <sazlin@gmail.com>
2014-08-25 20:46:12 -0700

    Removed debug task from celery.py

--- offerup_challenge_site/celery.py
+++ offerup_challenge_site/celery.py
@@ -15,8 +15,3 @@ app = Celery('offerup_challenge_site')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
